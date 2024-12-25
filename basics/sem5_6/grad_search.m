3b3b8b7954ec5aec61bb7a09fb0e9e39a0ff1474
Manarom <manarom@yandex.ru>
2024-12-25 08:58:33 +0300

    comments added

--- basics/sem5_6/grad_search.m
+++ basics/sem5_6/grad_search.m
@@ -3,8 +3,7 @@ function [x,Fval,ii,flag,search_history]=grad_search(x0,F,gradF,options)
 % входные аргументы:
 %                   x0 - стартовая точка
 %                   F - указатель на скалярную функцию векторного аргумента
-%                   gradF - указатель на функцию расчета градиента функции
-%                           F
+%                   gradF - указатель на функцию расчета градиента функции F
 %                   Опциональные аргументы в формате имя-значение
 %                   mu (optional)- амплитудный коэффициент, длина шага 
 %                   N (optional)- ограничение на число итераций
@@ -15,7 +14,7 @@ function [x,Fval,ii,flag,search_history]=grad_search(x0,F,gradF,options)
 %                   (минимизатор)
 %                   Fval - значение функции для найденного минимизатора
 %                   ii - число вычислений функции и ее градиента
-%                   flag - флажок критериев сходимости
+%                   flag - флажок критериев сходимости (изменение значения функции, изменение аргумента функции, модуль градиента)
 %                   search_history - матрица, у которой столбцы -
 %                   координаты в пространстве оптимизации, по которым ходил
 %                   алгоритм
@@ -30,19 +29,20 @@ function [x,Fval,ii,flag,search_history]=grad_search(x0,F,gradF,options)
     end
     ii=1;
     x=x0(:);mu = options.mu;N = options.N;tol = options.tol;
-    flag=[true true true];
+    flag=[true true true]; % флажок показателей сходимости
     Fval=F(x0);
-    is_return_search_history = false;
-    if nargout==5 % так как хранение всех точек может быть тяжелым
+    is_return_search_history = false; % флажок, который показывает нужно ли нам возвращать историю поиска
+    if nargout==5 % так как хранение всех точек может быть тяжелым делаем 
+    % матрицу только если функция вызвана с пятью выходными аргументами
         is_return_search_history =true;% если число выходных аргументов равно пяти, то значит нужно сохранить историю
         search_history = NaN(numel(x),N);% резервируем память под все точки алгоритма
-        search_history(:,1) = x0;
+        search_history(:,1) = x0;% записываем стартовую точку в историю поиска
     end
-    while ii<N && all(flag) % условием остановки служит достидение заданного числа итераций и проверка сходимости
+    while ii<N && all(flag) % условием остановки служит достижение заданного числа итераций и проверка сходимости
         x_previous=x;F_previous = Fval; % значения коордианты и функции на предыдущей итерации
         grad_value = gradF(x); % рассчитываем градиент функции
-        grad_norm = norm(grad_value);
-        if grad_norm==0
+        grad_norm = norm(grad_value); % модуль градиента 
+        if grad_norm==0 % если градиент равен нулю (внезапно), то мы уже в точке экстремума
             return
         end
         grad_direction = grad_value/norm(grad_value); % используем только направление градиента
@@ -53,8 +53,8 @@ function [x,Fval,ii,flag,search_history]=grad_search(x0,F,gradF,options)
         end
         % флажок проверки сходимости
         flag = [norm(Fval-F_previous)>tol ...%  изменение значения функции
-            norm(x_previous-x)>tol ...
-            grad_norm>tol]; %  изменение координаты
+            norm(x_previous-x)>tol ...%  изменение координаты
+            grad_norm>tol]; %  модуль градиента
         ii=ii+1; 
     end
     if is_return_search_history
