a31ae3c746c5c63d78b4f68f46a0d0a5610aa867
Roman Mironov <manarom@yandex.ru>
2025-02-11 21:13:54 +0300

    small edit to julia script

--- basics/sem7_10/julia_script_GLR.jl
+++ basics/sem7_10/julia_script_GLR.jl
@@ -18,10 +18,9 @@ data = DataFrame(Y=data_dict["y"][:,1], X1=data_dict["X"][:,1],X2=data_dict["X"]
 # linear regression using lm function from GLM
 first_order= lm(@formula(Y ~ X1 + X2),data) # first order linear regression
 second_order = lm(@formula(Y ~ X1 + X2 +X1^2 + X2^2),data) # second order linear regression
-
-
 # plotting results in 3d using Plots package functions
-f_select(modl::StatisticalModel)= begin
+# function for polynomial type selection
+function f_select(modl::StatisticalModel)
 	c = coef(modl)
 	N=length(c)
 	if N==3 
@@ -30,7 +29,9 @@ f_select(modl::StatisticalModel)= begin
 		return (x1,x2)->c[1] + c[2]*x1 + c[3]*x2 + c[4]*x1^2 + c[5]*x2^2
 	end
 end
+# plotting scatterd data
 p=scatter3d(data.X1,data.X2,data.Y)
+# iteration over regression order 
 for ord in (first_order,second_order)
     f=f_select(ord)
     plot!(range(extrema(data.X1)...,100),range(extrema(data.X2)...,100),f,st=:surface,camera=(-30,30),alpha=0.5)
